10d078723bd9264a1a079717d4876e310ce8e216
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-04-10 18:59:08 -0500

    Se implementa el constructor de produto para inicializar los atributos.

--- src/main/java/com/mycompany/fruver/Producto.java
+++ src/main/java/com/mycompany/fruver/Producto.java
@@ -5,4 +5,8 @@ package com.mycompany.fruver;
 public class Producto {
     String nombre;
     int cantidad;
+    public producto(String nombre, int cantidad){
+        this.nombre=nombre;
+        this.cantidad=cantidad;      
+    }
 }
