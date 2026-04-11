342c33f9237067422dd7a79a146d7f4e7c5a97cf
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-04-10 19:14:03 -0500

    intentamos hacer un intento para de insertar pero sin que se recorra la lista, pero nos marca el error

--- src/main/java/com/mycompany/fruver/ListaProductos.java
+++ src/main/java/com/mycompany/fruver/ListaProductos.java
@@ -3,4 +3,9 @@ package com.mycompany.fruver;
 
 public class ListaProductos {
     NodoProducto inicio;
+  public void insertar(Producto p) {
+    NodoProducto nuevo = new NodoProducto(p);
+    inicio = nuevo;
+   }  
+    
 }
