023fe4a08b7bbee82b8610fd8f342353f284ae6b
Laura Alexandra Herrera Espitia <est.lauraa.herrerae@unimilitar.edu.co>
2026-04-10 19:09:21 -0500

    se crea la clase ListaProducto

---
+++ src/main/java/com/mycompany/fruver/NodoProducto.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.fruver;
+
+public class NodoProducto {
+    Producto dato;
+    NodoProducto siguiente;
+  public NodoProducto(Producto dato) {
+    
+    this.dato = dato;
+    this.siguiente = null;
+  }
+}
