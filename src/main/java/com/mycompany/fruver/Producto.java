c2b3adc3348893d31c3519b4e298242b3ced817c
mrpae <mrpae@DESKTOP-PVEVJ2J>
2026-04-16 00:42:23 -0500

    se agrega en clase producto el metodo vender y para controlar la cantidad

--- src/main/java/com/mycompany/fruver/Producto.java
+++ src/main/java/com/mycompany/fruver/Producto.java
@@ -5,18 +5,20 @@ package com.mycompany.fruver;
 public class Producto {
     String nombre;
     int cantidad;
-    
+    //constructor
     public Producto(String nombre, int cantidad){
         this.nombre=nombre;
         this.cantidad=cantidad;      
     }
+    //metodo para vender producto
     public void vender(int cant) {
      if (cant <= cantidad) {
         cantidad -= cant;
+        System.out.println("Venta realizada. Cantidad restante: " + cantidad);
     } else {
         System.out.println("No hay suficiente cantidad");
     }
 
-    }
+  }
 
 }
