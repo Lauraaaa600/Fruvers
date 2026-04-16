76bd23fbe6f2724e056963c0f13ab6ed3815f795
est.sebastian.ariasr@unimilitar.edu.co <sebas@Sebastian>
2026-04-15 21:20:11 -0500

    se agregan cambios para ayudarnos con la libreria

--- src/main/java/com/mycompany/fruver/Fruver.java
+++ src/main/java/com/mycompany/fruver/Fruver.java
@@ -1,31 +1,34 @@
 
 package com.mycompany.fruver;
+import com.murcia.utils.*;
 
-public class Fruver {
+public class Fruver{
 
     public static void main(String[] args) {
-    System.out.println("--- Bienvenido a Fruver ---");
-    System.out.println("Elija una opcion:");
-    System.out.println("1: Mostrar producto");
-    System.out.println("2: Cliente compra");
-    System.out.println("3: Trabajador atiende cliente");
+        
+        System.out.println("--- Bienvenido a Fruver ---");
+        System.out.println("Elija una opcion:");
+        System.out.println("1: Mostrar producto");
+        System.out.println("2: Cliente compra");
+        System.out.println("3: Trabajador atiende cliente");
     
     ListaProductos lista= new ListaProductos();
     
-    lista.insertar(new Producto("Manzana",10));
-    lista.insertar(new Producto("Pera",5));
+        lista.insertar(new Producto("Manzana",4000));
+        lista.insertar(new Producto("Pera",3000));
     
     
-    int opcion = 2;
+    int opcion = Input.nextInt("Ingrese su opcion: ");
     
     switch (opcion) {
         case 1:
-    lista.mostrar();
-    break;
+            lista.mostrar();
+            break;
     
         case 2:
-            Cliente c= new Cliente("Juan");
-            Producto p = lista.buscar("Banano");
+            Cliente c= new Cliente(Input.nextLine("nombre del cliente: "));
+            Input.nextLine("");
+            Producto p = lista.buscar(Input.nextLine("nombre del producto: "));
             
             if(p !=null) {
                 c.comprar(p,2);
