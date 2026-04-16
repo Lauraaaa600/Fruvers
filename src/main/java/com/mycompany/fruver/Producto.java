634003b5abc08b0f6548bb579e644276039a65a2
mrpae <mrpae@DESKTOP-PVEVJ2J>
2026-04-16 01:06:03 -0500

    Decidimos agregarle precio al producto y se calcula total a pagar en el metodo vender

--- src/main/java/com/mycompany/fruver/Producto.java
+++ src/main/java/com/mycompany/fruver/Producto.java
@@ -5,20 +5,30 @@ package com.mycompany.fruver;
 public class Producto {
     String nombre;
     int cantidad;
+    double precio;
     //constructor
-    public Producto(String nombre, int cantidad){
+    public Producto(String nombre, int cantidad, double precio){
         this.nombre=nombre;
-        this.cantidad=cantidad;      
+        this.cantidad=cantidad;  
+        this.precio=precio;
     }
     //metodo para vender producto
-    public void vender(int cant) {
-     if (cant <= cantidad) {
-        cantidad -= cant;
-        System.out.println("Venta realizada. Cantidad restante: " + cantidad);
-    } else {
-        System.out.println("No hay suficiente cantidad");
-    }
+    public boolean vender(int cant) {
 
-  }
+        if (cant <= cantidad) {
+            cantidad -= cant;
+            double total = cant * precio;
+
+            System.out.println("Cantidad comprada: " + cant);
+            System.out.println("Precio unitario: " + precio);
+            System.out.println("Total a pagar: " + total);
+
+            return true; // venta exitosa
+        } else {
+            System.out.println("No hay suficiente cantidad disponible");
+            return false;
+        }
+
+    }
 
 }
